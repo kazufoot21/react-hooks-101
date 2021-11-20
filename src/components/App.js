8e90987c777680437ed6f1e37321a4e965063af2
kazufoot21 <kazuhiromaeda@maedatomohironoMacBook-Pro.local>
2021-11-20 09:30:43 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, {useReducer, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {events} from '../reducer';
 
 const App = () => {
+    const [state, dispatch] = useReducer(events, []);
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const addEvent = (e) => {
+        e.preventDefault();
+        // console.log({title, body});
+        dispatch({
+            type: 'CREATE_EVENT',
+            title,
+            body,
+        });
+        setTitle('');
+        setBody('');
+    };
+    console.log({state});
+
     return (
         <div className="container-fluid">
             <h4>Event Create Form</h4>
             <form>
                 <div className="form-group">
                     <label htmlFor="formEventTitle">Title</label>
-                    <input className="form-control" id="formEventTitle"></input>
+                    <input
+                        className="form-control"
+                        id="formEventTitle"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    ></input>
                 </div>
                 <div className="form-group">
                     <label htmlFor="formEventBody">Body</label>
                     <textarea
                         className="form-control"
                         id="formEventBody"
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
                     ></textarea>
                 </div>
 
-                <button className="btn btn-primary">Create Event</button>
+                <button className="btn btn-primary" onClick={addEvent}>
+                    Create Event
+                </button>
                 <button className="btn btn-danger">Delete All Events</button>
             </form>
 
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -15,7 +15,7 @@
 export const events = (state = [], action) => {
     switch (action.type) {
         case 'CREATE_EVENT':
-            const event = {title: action.type, body: action.body};
+            const event = {title: action.title, body: action.body};
             const length = state.length;
             const id = length === 0 ? 1 : state[length - 1].id + 1;
             return [...state, {id, ...event}];
