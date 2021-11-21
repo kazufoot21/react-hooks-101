441c708c04c5d4f225cc72a3806dc68c42c62460
kazufoot21 <kazuhiromaeda@maedatomohironoMacBook-Pro.local>
2021-11-21 09:49:23 +0900

    setup react Context

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-const CREATE_EVENT = 'CREATE_EVENT';
-const DELETE_EVENT = 'DELETE_EVENT';
-const DELETE_ALL_EVENT = 'DELETE_ALL_EVENT';
-
-export default DELETE_EVENT;
+export const CREATE_EVENT = 'CREATE_EVENT';
+export const DELETE_EVENT = 'DELETE_EVENT';
+export const DELETE_ALL_EVENT = 'DELETE_ALL_EVENT';
--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducer';
 import Events from './Events';
 import EventForm from './EventForm';
+import AppContext from '../contexts/AppContext';
+
+console.log({AppContext});
 
 const App = () => {
     const [state, dispatch] = useReducer(reducer, []);
     return (
-        <div className="container-fluid">
-            <EventForm state={state} dispatch={dispatch} />
-            <Events state={state} dispatch={dispatch} />
-        </div>
+        <AppContext.Provider value={'Hello, I am a Provider'}>
+            <div className="container-fluid">
+                <EventForm state={state} dispatch={dispatch} />
+                <Events state={state} dispatch={dispatch} />
+            </div>
+        </AppContext.Provider>
     );
 };
 
