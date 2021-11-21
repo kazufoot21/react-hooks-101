91ef18e6f1050d5a417d0b3e6ef8e84879574b1a
kazufoot21 <kazuhiromaeda@maedatomohironoMacBook-Pro.local>
2021-11-21 10:10:30 +0900

    solve prop drilling issue

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,13 @@ import Events from './Events';
 import EventForm from './EventForm';
 import AppContext from '../contexts/AppContext';
 
-console.log({AppContext});
-
 const App = () => {
     const [state, dispatch] = useReducer(reducer, []);
     return (
-        <AppContext.Provider value={'Hello, I am a Provider'}>
+        <AppContext.Provider value={{state, dispatch}}>
             <div className="container-fluid">
-                <EventForm state={state} dispatch={dispatch} />
-                <Events state={state} dispatch={dispatch} />
+                <EventForm />
+                <Events />
             </div>
         </AppContext.Provider>
     );
